9c9d41b66f617df881a0eb7c9294cc8b0e60b919
Eremeev Nikita <ena2000.11@gmail.com>
2025-11-21 22:37:00 +0500

    feat: add last message creation time to props, update styles and error display

--- client/src/widgets/chat-list/ChatList.vue
+++ client/src/widgets/chat-list/ChatList.vue
@@ -48,13 +48,13 @@
 </script>
 
 <template>
-    <div class="flex flex-col min-w-58 h-full border-r border-gray">
+    <div class="flex flex-col lg:min-w-120 min-w-100 h-full border-r border-gray">
         <div class="p-4 space-y-3 border-b border-gray">
             <div class="flex flex-col gap-1">
-                <p class="mb-1 text-xs text-dark uppercase">Your ID</p>
+                <p class="mb-1 text-xl text-dark font-bold uppercase">Your ID</p>
 
-                <div class="flex items-center gap-2">
-                    <span class="px-2 py-1 font-mono bg-lightgray rounded">
+                <div class="flex gap-2">
+                    <span class="flex items-center px-2 py-1 w-fit font-mono text-2xl bg-lightgray rounded">
                         {{ userStore.userId }}
                     </span>
 
@@ -68,24 +68,24 @@
                 </div>
             </div>
 
-            <div class="space-y-2">
+            <div class="flex gap-2 w-fit">
                 <input
                     v-model="search"
                     type="text"
                     placeholder="Enter user ID"
-                    class="w-full px-3 py-2 text-sm rounded border border-gray"
+                    class="w-full px-3 py-2 text-xl rounded border border-gray"
                     @keydown.enter.prevent="onSearch"
                 />
 
-                <p
-                    v-if="error"
-                    class="text-xs text-error"
-                >
-                    {{ error }}
-                </p>
-
                 <AppButton @click="onSearch"> Start chat </AppButton>
             </div>
+
+            <p
+                v-if="error"
+                class="text-xl text-error"
+            >
+                Error: {{ error }}
+            </p>
         </div>
 
         <div class="flex-1 overflow-y-auto">
@@ -95,6 +95,7 @@
                 :active="chat.id === chatStore.activeChatId"
                 :name="chatStore.otherUserName(chat)"
                 :lastMessage="chat.lastMessage"
+                :date="chat.updatedAt"
                 @click="chatStore.selectChat(chat.id)"
             />
         </div>
