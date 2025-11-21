8492d53e93ccc06e1177da000db1926a3931f541
Eremeev Nikita <ena2000.11@gmail.com>
2025-11-21 14:26:10 +0500

    feat: implement chat preview list item component

---
+++ client/src/widgets/chat-list/ui/ChatListItem.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+    defineProps<{
+        name: string
+        lastMessage: string
+        active?: boolean
+    }>()
+</script>
+
+<template>
+    <div
+        class="group flex items-center gap-3 px-4 py-3 hover:bg-accent cursor-pointer transition-colors select-none"
+        :class="active ? 'bg-accent' : ''"
+    >
+        <div class="size-10 rounded-full bg-warning"></div>
+
+        <div class="flex flex-col">
+            <span
+                class="font-medium group-hover:text-white transition-colors leading-none"
+                :class="active ? 'text-white' : 'text-dark'"
+            >
+                {{ name }}
+            </span>
+            <span
+                class="text-sm group-hover:text-white transition-colors leading-none"
+                :class="active ? 'text-white' : 'text-dark'"
+            >
+                {{ lastMessage }}
+            </span>
+        </div>
+    </div>
+</template>
