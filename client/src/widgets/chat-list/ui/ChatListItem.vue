362ab9181cedf4ab06dfe323e4f6127a03a85914
Eremeev Nikita <ena2000.11@gmail.com>
2025-11-21 22:32:28 +0500

    feat: enhance component to display time ago for last message and style update

--- client/src/widgets/chat-list/ui/ChatListItem.vue
+++ client/src/widgets/chat-list/ui/ChatListItem.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-    defineProps<{
+    import { useTimeAgo } from '@/shared/composables/useTimeAgo'
+
+    const props = defineProps<{
         name: string
         lastMessage: string
         active?: boolean
+        date: string | number | Date
     }>()
+
+    const timeAgoValue = useTimeAgo(props.date)
 </script>
 
 <template>
@@ -11,21 +16,30 @@
         class="group flex items-center gap-3 px-4 py-3 hover:bg-accent cursor-pointer transition-colors select-none"
         :class="active ? 'bg-accent' : ''"
     >
-        <div class="size-10 rounded-full bg-warning"></div>
+        <div class="flex-none lg:size-16 size-12 rounded-full bg-warning"></div>
 
-        <div class="flex flex-col">
-            <span
-                class="font-medium group-hover:text-white transition-colors leading-none"
+        <div class="flex justify-between gap-x-4 flex-1">
+            <div class="flex flex-col gap-2 w-[-webkit-fill-available]">
+                <span
+                    class="block lg:text-xl text-md font-medium group-hover:text-white transition-colors leading-none"
+                    :class="active ? 'text-white' : 'text-dark'"
+                >
+                    {{ name }}
+                </span>
+                <span
+                    class="block lg:text-lg text-xs group-hover:text-white transition-colors leading-none"
+                    :class="active ? 'text-white' : 'text-dark'"
+                >
+                    {{ lastMessage }}
+                </span>
+            </div>
+            <div
+                v-if="timeAgoValue"
+                class="flex items-center lg:text-xs text-[0.7rem] min-w-max"
                 :class="active ? 'text-white' : 'text-dark'"
             >
-                {{ name }}
-            </span>
-            <span
-                class="text-sm group-hover:text-white transition-colors leading-none"
-                :class="active ? 'text-white' : 'text-dark'"
-            >
-                {{ lastMessage }}
-            </span>
+                {{ timeAgoValue }}
+            </div>
         </div>
     </div>
 </template>
