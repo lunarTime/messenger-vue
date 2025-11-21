2a560d0f2d620cdaa2496def9a11497eba161f22
Eremeev Nikita <ena2000.11@gmail.com>
2025-11-21 14:45:51 +0500

    feat: implement message input with submit action

---
+++ client/src/widgets/message-input/MessageInput.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+    import { ref } from 'vue'
+    import { useMessageStore } from '@/entities/message/store/message.store'
+    import AppButton from '@/widgets/ui/AppButton.vue'
+
+    const msgStore = useMessageStore()
+    const text = ref('')
+
+    const send = () => {
+        if (!text.value.trim()) {
+            return
+        }
+
+        msgStore.sendMessage(text.value)
+        text.value = ''
+    }
+</script>
+
+<template>
+    <div class="flex gap-3 p-3 border-t">
+        <textarea
+            v-model="text"
+            type="text"
+            class="flex-1 px-3 py-1 border border-gray rounded"
+            placeholder="Type message..."
+            @keydown.enter.prevent="send"
+        />
+        <AppButton @click="send"> Send </AppButton>
+    </div>
+</template>
