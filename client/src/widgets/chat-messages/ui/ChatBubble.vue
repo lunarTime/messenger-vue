9f137a6ba95d2da0391d63a3c80f8645e1de3994
Eremeev Nikita <ena2000.11@gmail.com>
2025-11-21 23:59:39 +0500

    feat: enhance component to display formatted creation time and improve layout

--- client/src/widgets/chat-messages/ui/ChatBubble.vue
+++ client/src/widgets/chat-messages/ui/ChatBubble.vue
@@ -1,41 +1,70 @@
 <script setup lang="ts">
     import { computed } from 'vue'
+    import { formatTime } from '@/shared/utils/formatTime'
+    import { PencilIcon } from '@heroicons/vue/24/solid'
 
     const props = defineProps<{
         text: string
         variant: 'incoming' | 'outgoing'
         deleted?: boolean
+        createdAd: Date | string | number
         edited?: boolean
     }>()
 
-    const bubbleClass = computed(() => {
-        if (props.variant === 'outgoing') {
-            return 'self-end bg-accent text-white'
-        }
-
-        return 'self-start bg-lightgray text-dark'
-    })
+    const isOutgoing = computed(() => props.variant === 'outgoing')
+    const whiteDarkColorClass = computed(() => (isOutgoing.value ? 'text-white' : 'text-dark'))
+    const bubbleClass = computed(() =>
+        isOutgoing.value ? 'self-end bg-accent text-white' : 'self-start bg-lightgray text-dark'
+    )
+    const createdDateDisplay = computed(() => formatTime(props.createdAd))
 </script>
 
 <template>
     <div
-        class="max-w-[70%] py-1.5 px-2 rounded-bubble text-xs whitespace-pre-wrap"
+        class="max-w-200 w-fit pb-2 pt-3 px-3 text-xs rounded-bubble"
         :class="bubbleClass"
     >
         <template v-if="!deleted">
-            <span>
-                {{ text }}
-            </span>
-            <span
-                v-if="edited"
-                class="ml-2 text-xs text-gray"
-            >
-                (edited)
-            </span>
+            <div class="relative flex flex-col">
+                <div class="relative bottom-0.5 flex items-center self-end gap-1.5 w-fit min-w-max">
+                    <div
+                        v-if="edited"
+                        class="min-w-max w-fit ml-2 text-xs"
+                        :class="whiteDarkColorClass"
+                    >
+                        <PencilIcon
+                            class="lg:size-3 size-2 block"
+                            :class="whiteDarkColorClass"
+                        />
+                    </div>
+                    <div
+                        class="relative -top-0.5 min-w-max w-fit text-xs self-end"
+                        :class="whiteDarkColorClass"
+                    >
+                        {{ createdDateDisplay }}
+                    </div>
+                </div>
+                <div class="lg:text-lg text-md break-all">
+                    {{ text }}
+                </div>
+            </div>
         </template>
 
         <template v-else>
-            <span class="text-gray italic"> Message deleted </span>
+            <div class="flex flex-col">
+                <div
+                    class="relative -top-0.5 min-w-max w-fit text-xs self-end"
+                    :class="whiteDarkColorClass"
+                >
+                    {{ createdDateDisplay }}
+                </div>
+                <span
+                    class="lg:text-lg text-md italic"
+                    :class="whiteDarkColorClass"
+                >
+                    Message deleted
+                </span>
+            </div>
         </template>
     </div>
 </template>
