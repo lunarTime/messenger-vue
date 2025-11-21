a3c24307e4250aded50164d36a109d7312d4169a
Eremeev Nikita <ena2000.11@gmail.com>
2025-11-21 14:42:29 +0500

    feat(chat): implement chat message list

---
+++ client/src/widgets/chat-messages/ChatMessageList.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+    import { watch, nextTick } from 'vue'
+    import { useMessageStore } from '@/entities/message/store/message.store'
+    import ChatBubble from '@/widgets/chat-messages/ui/ChatBubble.vue'
+
+    const store = useMessageStore()
+
+    const userId = window.localStorage.getItem('userId') || ''
+
+    const scrollToBottom = () => {
+        nextTick(() => {
+            const el = document.getElementById('messages')
+
+            if (el) {
+                el.scrollTop = el.scrollHeight
+            }
+        })
+    }
+
+    watch(() => store.messages, scrollToBottom, { deep: true })
+</script>
+
+<template>
+    <div
+        id="messages"
+        class="flex flex-col flex-1 gap-2 p-4 overflow-y-auto"
+    >
+        <ChatBubble
+            v-for="m in store.messages"
+            :key="m.id"
+            :text="m.text"
+            :variant="m.userId === userId ? 'outgoing' : 'incoming'"
+        />
+    </div>
+</template>
