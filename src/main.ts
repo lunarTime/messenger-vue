20c5f1fdc2086e2aeeb348096cf5c60da2378f7b
Eremeev Nikita <ena2000.11@gmail.com>
2025-12-12 17:21:11 +0500

    feat: integrate PrimeVue and custom theme, enhance user store initialization and cleanup on unload

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,62 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { ws } from '@/shared/api/ws/wsClient'
+import { useUserStore } from '@/entities/user/store/user.store'
 import { useChatStore } from '@/entities/chat/store/chat.store'
 import { useMessageStore } from '@/entities/message/store/message.store'
-import { useUserStore } from '@/entities/user/store/user.store'
-import { router } from '@/app/providers/router'
+import { definePreset } from '@primeuix/themes'
+import ToastService from 'primevue/toastservice'
+import router from '@/app/providers/router'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 import App from '@/App.vue'
+import 'primeicons/primeicons.css'
+import '@/app/providers/firebase'
 import '@/assets/main.css'
 
 const app = createApp(App)
 const pinia = createPinia()
+const customTheme = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{orange.50}',
+            100: '{orange.100}',
+            200: '{orange.200}',
+            300: '{orange.300}',
+            400: '{orange.400}',
+            500: '{orange.500}',
+            600: '{orange.600}',
+            700: '{orange.700}',
+            800: '{orange.800}',
+            900: '{orange.900}',
+            950: '{orange.950}'
+        }
+    }
+})
 
 app.use(pinia)
 app.use(router)
+app.use(ToastService)
+app.use(PrimeVue, {
+    theme: {
+        preset: customTheme,
+        options: {
+            darkModeSelector: '.dark',
+            cssLayer: false
+        }
+    },
+    ripple: true
+})
 
 const userStore = useUserStore(pinia)
 const chatStore = useChatStore(pinia)
 const messageStore = useMessageStore(pinia)
 
-ws.on('open', () => {
-    userStore.register()
-    chatStore.loadChats()
+userStore.initAuth()
+
+window.addEventListener('beforeunload', () => {
+    userStore.setOfflineStatus()
+    chatStore.cleanup()
+    messageStore.cleanup()
 })
-ws.on('users_online', chatStore.setOnline)
-ws.on('chats', chatStore.setChats)
-ws.on('messages', messageStore.setMessages)
-ws.on('message', messageStore.addMessage)
 
 app.mount('#app')
