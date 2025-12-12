c0f19d24ed2b5dcbcdada6d0f2fb138864fcbd92
Eremeev Nikita <ena2000.11@gmail.com>
2025-12-12 17:41:38 +0500

    feat: integrate animated background component and theme management

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
     import { RouterView } from 'vue-router'
+    import { useTheme } from '@/shared/composables/useTheme'
+    import AnimateBG from '@/shared/ui/AnimateBG.vue'
+
+    useTheme()
 </script>
 
 <template>
+    <AnimateBG />
     <RouterView v-slot="{ Component }">
         <transition
             name="fade"
