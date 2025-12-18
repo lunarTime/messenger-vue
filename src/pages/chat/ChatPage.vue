8d48c99bd85265d1a897d7945fbd747cec47f316
Eremeev Nikita <ena2000.11@gmail.com>
2025-12-19 01:38:01 +0500

    feat: integrate splitter for layout, and improve UI structure

--- src/pages/chat/ChatPage.vue
+++ src/pages/chat/ChatPage.vue
@@ -1,32 +1,60 @@
 <script setup lang="ts">
-    import { computed } from 'vue'
+    import { onMounted, onUnmounted } from 'vue'
     import { useChatStore } from '@/entities/chat/store/chat.store'
-    import ChatHeader from '@widgets/chat-header/ChatHeader.vue'
-    import ChatList from '@widgets/chat-list/ChatList.vue'
-    import ChatMessageList from '@widgets/chat-messages/ChatMessageList.vue'
-    import MessageInput from '@widgets/message-input/MessageInput.vue'
+    import { useMessageStore } from '@/entities/message/store/message.store'
+    import ChatList from '@/widgets/chat-list/ChatList.vue'
+    import ChatHeader from '@/widgets/chat-header/ChatHeader.vue'
+    import ChatMessageList from '@/widgets/chat-messages/ChatMessageList.vue'
+    import MessageInput from '@/widgets/message-input/MessageInput.vue'
+    import Splitter from 'primevue/splitter'
+    import SplitterPanel from 'primevue/splitterpanel'
 
     const chatStore = useChatStore()
-    const selectedChat = computed(() => chatStore.activeChatId)
+    const messageStore = useMessageStore()
+
+    onMounted(() => {
+        chatStore.loadChats()
+    })
+
+    onUnmounted(() => {
+        chatStore.cleanup()
+        messageStore.cleanup()
+    })
 </script>
 
 <template>
-    <div class="flex h-screen w-full">
-        <ChatList />
+    <div class="flex h-screen">
+        <Splitter class="w-full">
+            <SplitterPanel class="max-w-3xl min-w-fit">
+                <ChatList />
+            </SplitterPanel>
+            <SplitterPanel class="flex min-w-fit">
+                <div class="flex flex-col flex-1">
+                    <div
+                        v-if="!chatStore.activeChat"
+                        class="flex items-center justify-center flex-1 text-muted"
+                    >
+                        <p class="lg:text-xl text-lg">Выберите чат для начала общения</p>
+                    </div>
 
-        <template v-if="selectedChat">
-            <div class="flex flex-col flex-1">
-                <ChatHeader class="border-b border-gray" />
-                <ChatMessageList class="flex-1 overflow-y-auto" />
-                <MessageInput class="border-t bg-white border-gray" />
-            </div>
-        </template>
-
-        <div
-            v-else
-            class="flex flex-1 items-center justify-center bg-white text-dark"
-        >
-            No chat selected - choose one
-        </div>
+                    <template v-else>
+                        <div class="flex flex-col h-full max-h-screen">
+                            <ChatHeader />
+                            <ChatMessageList />
+                            <MessageInput />
+                        </div>
+                    </template>
+                </div>
+            </SplitterPanel>
+        </Splitter>
     </div>
 </template>
+
+<style scoped>
+    :deep(.p-splitter) {
+        background: none !important;
+        backdrop-filter: blur(100px);
+        border: none !important;
+        border-radius: 0 !important;
+    }
+</style>
