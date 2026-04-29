4420d8bd68b3fcd6005f44c60b748e4c0efed342
Nick Tale <ena2000.11@gmail.com>
2026-04-29 21:43:18 +0500

    style: update ChatPage layout and enhance splitter gutter styles for improved UI

--- src/pages/chat/ChatPage.vue
+++ src/pages/chat/ChatPage.vue
@@ -32,13 +32,15 @@ onUnmounted(() => {
         <div class="flex flex-col flex-1">
           <div
             v-if="!chatStore.activeChat"
-            class="flex items-center justify-center flex-1 text-muted"
+            class="flex items-center justify-center flex-1 gap-4 m-4 ml-0 p-4 text-muted bg-(--p-primary-color)/20 dark:bg-white/10 rounded-xl"
           >
             <p class="lg:text-xl text-lg">Выберите чат для начала общения</p>
           </div>
 
           <template v-else>
-            <div class="flex flex-col h-full max-h-screen">
+            <div
+              class="flex flex-col h-[-webkit-fill-available] max-h-screen gap-4 m-4 ml-0 p-4 bg-(--p-primary-color)/20 dark:bg-white/10 rounded-xl"
+            >
               <ChatHeader />
               <ChatMessages />
               <MessageInput />
@@ -56,5 +58,27 @@ onUnmounted(() => {
   backdrop-filter: blur(100px);
   border: none !important;
   border-radius: 0 !important;
+}
+
+:deep(.p-splitter-gutter) {
+  position: relative;
+  width: 1.5rem;
+  background-color: transparent;
+  outline: none !important;
+  user-select: none;
+}
+
+:deep(.p-splitter-gutter-handle) {
+  background-color: var(--p-primary-color);
+  opacity: 0.2;
+  transition: opacity 0.2s;
+}
+
+:deep(.p-splitter-gutter:hover .p-splitter-gutter-handle) {
+  opacity: 0.5;
+}
+
+:deep(.p-splitter-gutter:active .p-splitter-gutter-handle) {
+  opacity: 1;
 }
 </style>
